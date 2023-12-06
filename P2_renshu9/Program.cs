fd1c4b05a8e95d848db7d71a1e769707ef7a6549
miki@oic.jp <miki@oic.jp>
2023-12-06 10:43:27 +0900

    select メソッドの作成

--- P2_renshu9/Program.cs
+++ P2_renshu9/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * SELECT
+ */
+using P2_renshu9.Models;
+
+select();
+
+void select()
+{
+    MemberContext memberContext = new MemberContext();
+    foreach (Member member in memberContext.Members)
+    {
+        Console.WriteLine($"{member.MemberName}さんの電話番号は{member.MemberTel}です。");
+    }
+    memberContext.Dispose();
+}
