5cf48e58ee09ce5c12f8480b485905c1d2868e45
miki@oic.jp <miki@oic.jp>
2023-12-06 10:41:59 +0900

    create メソッドの作成

--- P2_renshu9/Program.cs
+++ P2_renshu9/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore.Update.Internal;
+using P2_renshu9.Models;
+using System.Runtime.InteropServices;
+
+/*
+ * INSETRT
+ */
+create();
+
+void create()
+{
+    MemberContext memberContext = new MemberContext();
+    Member member = new Member();
+    member.MemberName = "自分の名前";
+    member.MemberTel = "自分の電話番号";
+    memberContext.Members.Add(member);
+    memberContext.SaveChanges();
+    memberContext.Dispose();
+}
+
