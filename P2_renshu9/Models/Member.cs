aaa64763831663e10daf17343389ca38e6788e34
miki@oic.jp <miki@oic.jp>
2023-12-06 10:40:05 +0900

    プロジェクト ファイルを追加します。

---
+++ P2_renshu9/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P2_renshu9.Models;
+
+public partial class Member
+{
+    public int MemberId { get; set; }
+
+    public string? MemberName { get; set; }
+
+    public string? MemberTel { get; set; }
+}
